5741b8244d7e22740fc5f056f311f928752bdb30
Whittom <Nathan.Whittom@rjuhsd.int>
2022-01-25 12:46:03 -0800

    Ask user for Birthday info

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # birthday
+
+ask the user for their birthday, montha and year
+
+*commit/push
+*trade
+*Fork
+clone
+*add a feature that responds to the users age
+*commit 
+*push
+*pull request
---
+++ birthday.py
@@ -0,0 +1,9 @@
+import datetime
+
+
+print("Welcome,")
+m = input("Enter your Birth Month:")
+d = input("Enter your Birth Day:")
+y = input("Enter your Birth year:")
+
+print("You were born on:"+ m +"," + d +","+ y)
