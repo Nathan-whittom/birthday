ef60fbc8b043904c0a18d12ddb25ae35661de513
Reppas <James.Reppas@rjuhsd.int>
2022-01-26 12:19:30 -0800

    Tons of additions such as the date being added

--- birthday.py
+++ birthday.py
@@ -1,5 +1,4 @@
-import datetime
-
+from datetime import date
 
 print("Welcome,")
 m = input("Enter your Birth Month:")
@@ -7,3 +6,8 @@ d = input("Enter your Birth Day:")
 y = input("Enter your Birth year:")
 
 print("You were born on:"+ m +"," + d +","+ y)
+print("Happy Birthday " * 3)
+print("Happy birthday to you")
+
+td = date.today()
+print(td)
